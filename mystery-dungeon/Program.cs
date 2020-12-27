c5f0da328cf37034e1b81bd23be5c6660fe8a831
umaumax <umauma7083@gmail.com>
2020-12-27 15:00:35 +0900

    Update field generator

--- mystery-dungeon/Program.cs
+++ mystery-dungeon/Program.cs
@@ -2,30 +2,8 @@
 
 class Program {
     static void Main(string[] args) {
-        int x = 120;
-        int y = 30;
-        int padding = 8;
-        Field field = new Field(x, y);
-        var rand = new Random();
-        int rx1 = rand.Next(0 + padding, x - padding - 1 + 1);
-        int ry1 = rand.Next(0 + padding, y - padding - 1 + 1);
-        int w = rand.Next(4, 16);
-        int h = rand.Next(4, 10);
-        field.CreateRoomBasedCenter(rx1, ry1, w, h);
-        for (int i = 0; i < 10; i++) {
-            int rx2 = rand.Next(0 + padding, x - padding - 1 + 1);
-            int ry2 = rand.Next(0 + padding, y - padding - 1 + 1);
-            field.CreatePath(rx1, ry1, rx2, ry2);
-            w = rand.Next(4, 16);
-            h = rand.Next(4, 10);
-            var room = field.CreateRoomBasedCenter(rx2, ry2, w, h);
-            Console.WriteLine("room {0}, {1}, {2}, {3}", rx2, ry2, w, h);
-            if (room) {
-                Console.WriteLine("creat room");
-            }
-            rx1 = rx2;
-            ry1 = ry2;
-        }
+        var fieldGenerator = new FieldGenerator();
+        var field = fieldGenerator.Generate();
         field.Print();
     }
 }
--- mystery-dungeon/map_generator.cs
+++ mystery-dungeon/map_generator.cs
@@ -41,25 +41,53 @@ class Field {
             return false;
         }
         // WARN: below code is redundant
-        for (int i = sx; i <= ex; i++) {
-            var chip = new Chip();
-            chip.type = ChipType.RoadFloor;
-            chip_map[i, sy] = chip;
+        var rand = new Random();
+        if (rand.Next(0, 1 + 1) == 0) {
+            CreateXPath(sx, ex, sy);
+            CreateYPath(sy, ey, ex);
+        } else {
+            CreateYPath(sy, ey, sx);
+            CreateXPath(sx, ex, ey);
         }
-        for (int i = ex; i <= sx; i++) {
-            var chip = new Chip();
-            chip.type = ChipType.RoadFloor;
-            chip_map[i, sy] = chip;
+        return true;
+    }
+    public bool CreateXPath(int sx, int ex, int by) {
+        if (sx < 0 || ex < 0 || sx >= x || ex >= x || by < 0 || by >= y) {
+            // overflow from map
+            return false;
         }
-        for (int j = sy; j <= ey; j++) {
-            var chip = new Chip();
-            chip.type = ChipType.RoadFloor;
-            chip_map[ex, j] = chip;
+        if (sx <= ex) {
+            for (int i = sx; i <= ex; i++) {
+                var chip = new Chip();
+                chip.type = ChipType.RoadFloor;
+                chip_map[i, by] = chip;
+            }
+        } else {
+            for (int i = ex; i <= sx; i++) {
+                var chip = new Chip();
+                chip.type = ChipType.RoadFloor;
+                chip_map[i, by] = chip;
+            }
         }
-        for (int j = ey; j <= sy; j++) {
-            var chip = new Chip();
-            chip.type = ChipType.RoadFloor;
-            chip_map[ex, j] = chip;
+        return true;
+    }
+    public bool CreateYPath(int sy, int ey, int bx) {
+        if (sy < 0 || ey < 0 || sy >= y || ey >= y || bx < 0 || bx >= x) {
+            // overflow from map
+            return false;
+        }
+        if (sy <= ey) {
+            for (int j = sy; j <= ey; j++) {
+                var chip = new Chip();
+                chip.type = ChipType.RoadFloor;
+                chip_map[bx, j] = chip;
+            }
+        } else {
+            for (int j = ey; j <= sy; j++) {
+                var chip = new Chip();
+                chip.type = ChipType.RoadFloor;
+                chip_map[bx, j] = chip;
+            }
         }
         return true;
     }
@@ -95,7 +123,8 @@ class Field {
                         Console.Write(" ");
                         break;
                     case ChipType.RoadFloor:
-                        Console.Write(".");
+                        Console.Write(" ");
+                        // Console.Write(".");
                         break;
                     case ChipType.Wall:
                         Console.Write("*");
@@ -107,5 +136,41 @@ class Field {
             }
             Console.WriteLine();
         }
+    }
+}
+
+class FieldGenerator {
+    public int xsize;
+    public int ysize;
+    public int padding;
+    public FieldGenerator() {
+        xsize = 120;
+        ysize = 30;
+        padding = 8;
+    }
+
+    public Field Generate() {
+        var field = new Field(xsize, ysize);
+        var rand = new Random();
+        int rx1 = rand.Next(0 + padding, xsize - padding - 1 + 1);
+        int ry1 = rand.Next(0 + padding, ysize - padding - 1 + 1);
+        int w = rand.Next(4, 16);
+        int h = rand.Next(4, 10);
+        field.CreateRoomBasedCenter(rx1, ry1, w, h);
+        for (int i = 0; i < 10; i++) {
+            int rx2 = rand.Next(0 + padding, xsize - padding - 1 + 1);
+            int ry2 = rand.Next(0 + padding, ysize - padding - 1 + 1);
+            field.CreatePath(rx1, ry1, rx2, ry2);
+            w = rand.Next(4, 16);
+            h = rand.Next(4, 10);
+            var room = field.CreateRoomBasedCenter(rx2, ry2, w, h);
+            Console.WriteLine("room {0}, {1}, {2}, {3}", rx2, ry2, w, h);
+            if (room) {
+                Console.WriteLine("creat room");
+            }
+            rx1 = rx2;
+            ry1 = ry2;
+        }
+        return field;
     }
 }
